4509790351a1c06f2b835c8d0cdcd3b316e1be64
juror.yu <juror.yu@mart.blue>
2019-05-27 15:18:58 +0800

    ForeachTest add

--- src/main/java/com/zhyyu/learn/se/stream/ForeachTest.java
+++ src/main/java/com/zhyyu/learn/se/stream/ForeachTest.java
@@ -7,7 +7,8 @@ import java.util.List;
  * java8 foreach 测试
  * <pre>
  * 1. stream() 会中断之后循环
- * 2. parallelStream() 不会中断之后循环
+ * 2. parallelStream() 不会中断之后循环		// 20190527, 依然会中断, 未执行循环不执行, 有随机性
+ * 3. parallelStream().forEach 需要等到循环完成后才能执行下条语句
  * <pre>
  * @author Administrator
  *
@@ -15,21 +16,30 @@ import java.util.List;
 public class ForeachTest {
 
 	public static void main(String[] args) {
+//		List<Integer> intList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 		List<Integer> intList = Arrays.asList(1, 2, 3, 4, 5);
 		/*for (Integer integer : intList) {
 			printInt(integer);
 		}*/
 		
 		intList.stream().forEach(integer -> printInt(integer));
+		System.out.println("stream...");
+
 		intList.parallelStream().forEach(integer -> printInt(integer));
+		System.out.println("parallelStream...");
 	}
 	
 	public static void printInt(Integer integer) {
-		if (integer == 3) {
-			throw new RuntimeException();
-		}
+//		if (integer == 3) {
+//			throw new RuntimeException();
+//		}
 		
 		System.out.println(integer);
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+		}
 	}
 	
 }

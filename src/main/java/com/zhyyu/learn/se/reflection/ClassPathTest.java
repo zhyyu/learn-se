4e266f652ccf1d24d296d09e3b5972b708213657
juror.yu <juror.yu@mart.blue>
2019-06-04 15:16:52 +0800

    获取指定包下所有class

---
+++ src/main/java/com/zhyyu/learn/se/reflection/ClassPathTest.java
@@ -0,0 +1,64 @@
+package com.zhyyu.learn.se.reflection;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.List;
+
+/**
+ * 获取指定包下所有class
+ * @author juror
+ * @datatime 2019/6/4 15:04
+ */
+public class ClassPathTest {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        // https://www.mkyong.com/java/how-to-print-out-the-current-project-classpath/
+        /*ClassLoader cl = ClassLoader.getSystemClassLoader();
+        URL[] urls = ((URLClassLoader)cl).getURLs();
+        for(URL url: urls){
+            System.out.println(url.getFile());
+        }*/
+
+
+        // https://dzone.com/articles/get-all-classes-within-package
+        String packageName = "com.zhyyu";
+
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        assert classLoader != null;
+        String path = packageName.replace('.', '/');
+        Enumeration resources = classLoader.getResources(path);
+        List<File> dirs = new ArrayList();
+        while (resources.hasMoreElements()) {
+            URL resource = (URL) resources.nextElement();
+            dirs.add(new File(resource.getFile()));
+        }
+        ArrayList classes = new ArrayList();
+        for (File directory : dirs) {
+            classes.addAll(findClasses(directory, packageName));
+        }
+
+        System.out.println(classes);
+    }
+
+    private static List findClasses(File directory, String packageName) throws ClassNotFoundException {
+        List classes = new ArrayList();
+        if (!directory.exists()) {
+            return classes;
+        }
+        File[] files = directory.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                assert !file.getName().contains(".");
+                classes.addAll(findClasses(file, packageName + "." + file.getName()));
+            } else if (file.getName().endsWith(".class")) {
+                classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
+            }
+        }
+        return classes;
+    }
+
+}

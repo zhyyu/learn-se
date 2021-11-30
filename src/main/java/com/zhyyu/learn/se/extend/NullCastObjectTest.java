e11b8ca02747be22a45d0edc9e48a5a51b17bcdd
todo <todo@todo>
2021-11-30 10:38:35 +0800

    NullCastObjectTest

---
+++ src/main/java/com/zhyyu/learn/se/extend/NullCastObjectTest.java
@@ -0,0 +1,10 @@
+package com.zhyyu.learn.se.extend;
+
+public class NullCastObjectTest {
+
+    public static void main(String[] args) {
+        Object obj = (Object)null;
+        System.out.println(obj == null);
+    }
+
+}
